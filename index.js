272e7d7cdc12390b3e5947194a750416f1a2dc1c
estefysol03 <estefysol03@gmail.com>
2023-10-23 20:34:46 -0300

    cambios carrousel

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-let currentIndex = 0;
-const images = document.querySelectorAll('#image-list li');
-const carouselContainer = document.getElementById('carousel-container');
-const imageList = document.getElementById('image-list');
 
-function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;//indice actual + 1.
-    updateCarousel();
-}
 
-function updateCarousel() {
-    const offset = -currentIndex * 100;   //Actualiza el carousel.
-    imageList.style.transform = `translateX(${offset}%)`;
-}
 
-// Cambia de imagen cada 3 segundos.
-setInterval(nextImage, 3000);
+   // Carrusel
+
+   let currentIndex = 0;  //indice actual.
+
+        function showImage(index) {   //nos muestra las imagenes.
+            const imageList = document.getElementById("image-list");
+            imageList.style.transform = `translateX(-${index * 400}px)`; //trae la lista y su estilo.
+        }
+
+        function nextImage() {
+            currentIndex = (currentIndex + 1) % 9;   
+            showImage(currentIndex);
+        }
+
+        function prevImage() {
+            currentIndex = (currentIndex - 1 + 9) % 9; // 9 Cantidad de imagenes que tengo.
+            showImage(currentIndex);
+        }
+ 
+
+
+
+
+//FORMULARIO.
+
+
+    
+    function validarFormulario() {
+        var nombre = document.getElementById('nombre').value;
+        var email = document.getElementById('email').value;
+        var mensaje = document.getElementById('mensaje').value;
+        
+       
+        if (nombre == "") {
+            errores.push("falta el nombre")
+            campo_error=nombre;S
+        }
+        return false;
+    }
+    
+  
+          
+   
+    
+
