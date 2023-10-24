84e14d07d893ee35629ca48337da90636d08963e
estefysol03 <estefysol03@gmail.com>
2023-10-24 18:17:48 -0300

    cambios en el formulario

--- index.js
+++ index.js
@@ -22,24 +22,72 @@
  
 
 
-
-
 //FORMULARIO.
 
 
-    
-    function validarFormulario() {
-        var nombre = document.getElementById('nombre').value;
-        var email = document.getElementById('email').value;
-        var mensaje = document.getElementById('mensaje').value;
-        
-       
-        if (nombre == "") {
-            errores.push("falta el nombre")
-            campo_error=nombre;S
+function validarFormulario() {
+    const envioForm = document.getElementById("envioForm");
+    const ltaErrores = document.getElementById("ltaErrores");
+
+//traigo los valores,
+    let f_nombre = document.getElementById("f_nombre");
+    let nombre = f_nombre.value.trim();
+    let f_email = document.getElementById("f_email");
+    let email = f_email.value.trim();
+    let f_mensaje = document.getElementById("f_mensaje");
+    let mensaje = f_mensaje.value.trim();
+
+    let errores = []; //funcion en donde marca si la lista esta vacia no hay errores.
+    let campo_error = null;  //campo donde marca el error.
+    let frm = document.getElementById("frm");
+
+    for (v of frm.children) {
+        v.classList.remove("error");
+    }
+
+
+    //valido cada campo.
+    if (nombre == "") {     //si no cumple conlo que pide marca error.
+        errores.push("Falta el nombre");
+        campo_error = f_nombre;
+        f_nombre.classList.add("error");
+    }
+
+    if (email == "") {
+        errores.push("Falta el email");
+        campo_error = f_email;
+        f_email.classList.add("error");
+    }
+
+    if (mensaje == "") {
+        errores.push("Falta el mensaje");
+        campo_error = f_mensaje;
+        f_mensaje.classList.add("error");
+    }
+
+    ltaErrores.innerHTML = ""; //a todo lista errores lo piso.
+
+    if (errores.length > 0) {  //aca si hay error,creo un (li)le seteo el texto
+        for (let i = 0; i < errores.length; i++) {
+            let li = document.createElement("li");
+            li.innerHTML = errores[i];
+            ltaErrores.appendChild(li);
+        }
+        if (campo_error != null) {
+            campo_error.focus();
         }
         return false;
     }
+    let mje = `Hola ${nombre} ${email}, ¡Recibimos tu MENSAJE ${mensaje}!`;
+    envioForm.innerHTML = mje;
+    return false;
+}
+          
+
+
+
+
+    
     
   
           
