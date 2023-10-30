9e3764d722c3bb54f08c52194bec5b3176d69154
estefysol03 <estefysol03@gmail.com>
2023-10-30 16:23:53 -0300

    cambios

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 
 
-
-   // Carrusel
-
-   let currentIndex = 0;  //indice actual.
-
-        function showImage(index) {   //nos muestra las imagenes.
-            const imageList = document.getElementById("image-list");
-            imageList.style.transform = `translateX(-${index * 400}px)`; //trae la lista y su estilo.
-        }
-
-        function nextImage() {
-            currentIndex = (currentIndex + 1) % 9;   
-            showImage(currentIndex);
-        }
-
-        function prevImage() {
-            currentIndex = (currentIndex - 1 + 9) % 9; // 9 Cantidad de imagenes que tengo.
-            showImage(currentIndex);
-        }
+//Carrousel
  
+        var images = [
+            "img/imagen1.jpg",
+            "img/imagen2.jpg",
+            "img/imagen3.jpg",
+            "img/imagen4.jpg",
+            "img/imagen5.jpg",
+            "img/imagen6.jpg",
+            "img/imagen7.jpg",
+            "img/imagen8.jpg",
+            "img/imagen9.jpg"
+        ];
 
+        var currentIndex = 0;
+        var imageList = document.getElementById("image-list");
+
+        // Funcion para mostrar la imagen actual.
+        function showImage() { 
+            imageList.innerHTML = "<li><img src='" + images[currentIndex] + "' alt='Imagen " + (currentIndex + 1) + "'></li>";
+        }
+
+        // Funcion para avanzar a la siguiente imagen
+        function nextImage() {
+            currentIndex = (currentIndex + 1) % images.length; //indice actual +1.
+            showImage();
+        }
+
+        // Funcion para retroceder a la imagen anterior
+        function prevImage() {
+            currentIndex = (currentIndex - 1 + images.length) % images.length;
+            showImage();
+        }
+
+        // Mostrar la primera imagen al cargar la pagina
+        showImage();
 
 //FORMULARIO.
 
@@ -32,6 +47,8 @@ function validarFormulario() {
 //traigo los valores,
     let f_nombre = document.getElementById("f_nombre");
     let nombre = f_nombre.value.trim();
+    let f_apellido = document.getElementById("f_apellido");
+    let apellido = f_apellido.value.trim();
     let f_email = document.getElementById("f_email");
     let email = f_email.value.trim();
     let f_mensaje = document.getElementById("f_mensaje");
@@ -40,6 +57,7 @@ function validarFormulario() {
     let errores = []; //funcion en donde marca si la lista esta vacia no hay errores.
     let campo_error = null;  //campo donde marca el error.
     let frm = document.getElementById("frm");
+    
 
     for (v of frm.children) {
         v.classList.remove("error");
@@ -51,6 +69,17 @@ function validarFormulario() {
         errores.push("Falta el nombre");
         campo_error = f_nombre;
         f_nombre.classList.add("error");
+    }
+    else if (nombre.length > f_nombre.maxLength) {
+        errores.push("El nombre excede la longitud maxima permitida.");
+        campo_error = f_nombre;
+        f_nombre.classList.add("error");
+    }
+
+    if (apellido == "") {     //si no cumple con lo que pide marca error.
+        errores.push("Falta el apellido");
+        campo_error = f_nombre;
+        f_apellido.classList.add("error");
     }
 
     if (email == "") {
@@ -65,7 +94,7 @@ function validarFormulario() {
         f_mensaje.classList.add("error");
     }
 
-    ltaErrores.innerHTML = ""; //a todo lista errores lo piso.
+    ltaErrores.innerHTML = ""; //a toda la lista de errores lo piso.
 
     if (errores.length > 0) {  //aca si hay error,creo un (li)le seteo el texto
         for (let i = 0; i < errores.length; i++) {
@@ -78,9 +107,23 @@ function validarFormulario() {
         }
         return false;
     }
-    let mje = `Hola ${nombre} ${email}, ¡Recibimos tu MENSAJE ${mensaje}!`;
+
+
+
+    let mje = `Hola ${nombre} ${apellido} Recibimos desde el correo ${email}, tu Mensaje! Gracias por tus Palabras! te esperamos Pronto!!`;
     envioForm.innerHTML = mje;
+   
+
+    if (errores.length === 0) {
+        // Limpio los campos despues de enviar el formulario
+        f_nombre.value = "";
+        f_apellido.value = "";
+        f_email.value = "";
+        f_mensaje.value = "";
+    }
+
     return false;
+
 }
           
 
